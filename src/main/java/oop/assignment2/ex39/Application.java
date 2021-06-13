74c7cb8dd5b61ee57e835e914ef4139b8bcd6548
agalvezv <43121059+agalvezv@users.noreply.github.com>
2021-06-13 18:05:28 -0400

    Exercise 39

---
+++ src/main/java/oop/assignment2/ex39/Application.java
@@ -0,0 +1,10 @@
+package oop.assignment2.ex39;
+
+public class Application {
+    public static void main(String[] args) {
+        TableOrganization newMap = new TableOrganization();
+        newMap.initLists();
+        newMap.rearrangeLists();
+        newMap.mapLists();
+    }
+}
---
+++ src/main/java/oop/assignment2/ex39/TableOrganization.java
@@ -0,0 +1,165 @@
+package oop.assignment2.ex39;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class TableOrganization {
+
+    static ArrayList<String> firstName = new ArrayList<String>();
+    static ArrayList<String> lastName = new ArrayList<String>();
+    static ArrayList<String> sortList = new ArrayList<String>();
+    static ArrayList<String> position = new ArrayList<String>();
+    static ArrayList<String> sepDate = new ArrayList<String>();
+    static ArrayList<String> updatedList = new ArrayList<String>();
+
+    public static void initLists() {
+
+        firstName.add("John");
+        firstName.add("Tou");
+        firstName.add("Michaela");
+        firstName.add("Jake");
+        firstName.add("Jacquelyn");
+        firstName.add("Sally");
+
+        lastName.add("Johnson");
+        lastName.add("Xiong");
+        lastName.add("Michaelson");
+        lastName.add("Jacobson");
+        lastName.add("Jackson");
+        lastName.add("Webber");
+
+        position.add("Manager");
+        position.add("Software Engineer");
+        position.add("District Manager");
+        position.add("Programmer");
+        position.add("DBA");
+        position.add("Web Developer");
+
+        sepDate.add("2016-12-31");
+        sepDate.add("2016-10-05");
+        sepDate.add("2015-12-19");
+        sepDate.add("");
+        sepDate.add("");
+        sepDate.add("2015-12-18");
+
+        sortList.add("Johnson");
+        sortList.add("Xiong");
+        sortList.add("Michaelson");
+        sortList.add("Jacobson");
+        sortList.add("Jackson");
+        sortList.add("Webber");
+    }
+
+    public static void rearrangeLists()
+    {
+        Collections.sort(sortList);
+        int size = sortList.size();
+        //String space1 = "";
+        //String space2 = "";
+        //String space3 = "";
+        String[] space1= new String[20];
+        String[] space2= new String[20];
+        String[] space3= new String[20];
+
+        int sizeName=0;
+        int sizePosition=0;
+        int sizeSD=0;
+
+        String[] sizeNameS= new String[20];
+        String[] sizePositionS= new String[20];
+        String[] sizeSDS= new String[20];
+
+        String compare = "--------------------";
+
+
+        for(int i=0;i<size;i++)
+        {
+            for(int j=0;j<size;j++)
+            {
+                if(sortList.get(i)== lastName.get(j))
+                {
+                    sizeName=0;
+                    sizePosition=0;
+                    sizeSD=0;
+                    sizeNameS[j]="";
+
+                    sizePositionS[j]="";
+
+                    sizeSDS[j]="";
+
+                    space1[j] = "";
+                    space2[j] = "";
+                    space3[j] = "";
+
+                    sizeNameS[j]=(String)firstName.get(j) + " "+ (String)lastName.get(j);
+                    sizeName=sizeNameS[j].length();
+
+                    sizePositionS[j]= (String)position.get(j);
+                    sizePosition=sizePositionS[j].length();
+
+                    sizeSDS[j]= (String)sepDate.get(j);
+                    sizeSD=sizeSDS[j].length();
+
+                    for(int k=0;k<compare.length();k++)
+                    {
+
+                        if(sizeName<compare.length())
+                        {
+                            space1[j]= space1[j]+" ";
+                            sizeName++;
+
+                        }
+                        if(sizePosition<compare.length())
+                        {
+                            space2[j]= space2[j]+" ";
+                            sizePosition++;
+
+                        }
+                        if(sizeSD<compare.length())
+                        {
+                            space3[j]= space3[j]+" ";
+                            sizeSD++;
+
+                        }
+
+
+
+                    }
+                    updatedList.add(firstName.get(j)+" " +lastName.get(j)+space1[j]+"| "+position.get(j)+space2[j]+"| "+sepDate.get(j));
+                }
+
+            }
+        }
+        //System.out.println(sepDate.get(2));
+        //System.out.println(updatedList.get(2));
+    }
+    public static void mapLists(){
+        int size = updatedList.size();
+        String temp= "";
+        HashMap<Integer,String> mymap = new HashMap<Integer,String>();
+        /*
+        System.out.println("Name                | Position          | Separation Date");
+        System.out.println("--------------------|-------------------|----------------");
+        for(int i=0; i<size;i++)
+        {
+            System.out.println(updatedList.get(i));
+            //mymap.put(1,(String)updatedList.get(i));
+            //System.out.println(mymap.containsValue(3));
+        }
+
+         */
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Name                | Position            | Separation Date");
+        System.out.println("--------------------|---------------------|----------------");
+        for(int i=0; i<size;i++)
+        {
+            temp = updatedList.get(i);
+            mymap.put(i,temp);
+            System.out.println(mymap.get(i));
+        }
+
+    }
+
+}
